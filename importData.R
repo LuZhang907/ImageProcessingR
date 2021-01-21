60fbcfcc18371c1cad231e9fc6ab7dbde67093d7
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2021-01-21 09:42:08 -0600

    make some correction: market prices=closing prices; plot denoised spectrum

--- importData.R
+++ importData.R
@@ -31,8 +31,7 @@ SPX140411 <- subset(SPX,DateTime >= "2014-04-11 09:30:00 " & DateTime <= "2014-0
 # calculate prices and returns
 
 # prices=avergae(O,H,L,C)
-x<-cbind(SPX140411$Open,SPX140411$High,SPX140411$Low, SPX140411$Close)
-prices<-rowMeans(x)
+prices<-SPX140411$Close
 
 # return=log(P_i)-log(P_i-1)
 return<-rep(0,length(prices)-1)
@@ -80,7 +79,7 @@ library(remotes)
 #install_version("wmtsa", "2.0-3")
 library(wmtsa)
 DWTprice<-wavShrink(prices, wavelet="d4",
-          n.level=2, #level=5 is too smoothy
+          n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
           shrink.fun="soft", thresh.fun="adaptive")
 
 # original signal v.s. denoised signal
@@ -126,8 +125,249 @@ my.w<-analyze.wavelet(my.data, "x",
                       make.pval = FALSE,n.sim = 10)
 
 #plot the wavelet power spectrum
-wt.image(my.w, color.key = "interval",n.levels=5,
+wt.image(my.w, color.key = "interval",n.levels=250,
          legend.params = list(lab="wavelet power levels",mar=4.7, label.digits=2)
          )
 
+# power spectrum-logReturn
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/50,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum
+wt.image(my.w, color.key = "interval",n.levels=5,
+         legend.params = list(lab="wavelet power levels",mar=4.7, label.digits=2)
+)
+
+##################################################################
+##################       2009-04-08        #######################
+
+#Extract the data on 2009-04-08
+SPX090408 <- subset(SPX,DateTime >= "2009-04-08 09:30:00 " & DateTime <= "2009-04-08 16:00:00")
+
+# calculate prices and returns
+
+prices<-SPX090408$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+##################################################################
+##################       2011-04-06        #######################
+
+#Extract the data on 2011-04-06
+SPX110406 <- subset(SPX,DateTime >= "2011-04-06 09:30:00 " & DateTime <= "2011-04-06 16:00:00")
+
+# calculate prices and returns
+
+prices<-SPX110406$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+##################################################################
+##################       2013-04-10        #######################
+
+#Extract the data on 2013-04-10
+SPX130410 <- subset(SPX,DateTime >= "2013-04-10 09:30:00 " & DateTime <= "2013-04-10 16:00:00")
+
+# calculate prices and returns
+
+prices<-SPX130410$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+##################################################################
+##################       2015-04-08        #######################
+
+#Extract the data on 2015-04-08
+SPX150408 <- subset(SPX,DateTime >= "2015-04-08 09:30:00 " & DateTime <= "2015-04-08 16:00:00")
+
+# calculate prices and returns
+
+prices<-SPX150408$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+##################################################################
+##################       2017-04-12        #######################
+
+#Extract the data on 2017-04-12
+SPX170412 <- subset(SPX,DateTime >= "2017-04-12 09:30:00 " & DateTime <= "2017-04-12 16:00:00")
+
+# calculate prices and returns
+
+prices<-SPX170412$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+
+##################################################################
+##################       2019-04-10        #######################
+
+#Extract the data on 2017-04-12
+SPX190410 <- subset(SPX,DateTime >= "2019-04-10 09:30:00 " & DateTime <= "2019-04-10 16:00:00")
+
+# calculate prices and returns
+prices<-SPX190410$Close
+DWTprice<-wavShrink(prices, wavelet="d4",
+                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
+                    shrink.fun="soft", thresh.fun="adaptive")
+
+# original signal v.s. denoised signal
+tsplot(prices, ylab="price", lwd=2, col=rgb(0.2,0.4,0.8))
+lines(DWTprice, lwd=2, col=rgb( 0.8,0.5,0.3) )
+legend('topright', col=c(rgb(0.2,0.4,0.8),  rgb( 0.8,0.5,0.3)), lwd=2, 
+       legend=c("orginal signal", "denoised signal"), bg='white') 
+
+#plot denoised power spectrum
+logReturn<-rep(0,length(DWTprice)-1)
+for (i in 1: (length(DWTprice)-1)){
+  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+}
+
+my.data<-data.frame(x=logReturn)
+my.w<-analyze.wavelet(my.data, "x",
+                      loess.span = 0,
+                      dt=1,dj=1/250,
+                      lowerPeriod = 2,
+                      upperPeriod = 256,
+                      make.pval = FALSE,n.sim = 10)
+
+#plot the wavelet power spectrum-prices
+wt.image(my.w, color.key = "interval",n.levels=250,
+         legend.params = list(lab="wavelet power levels",mar=4.7))
+
+
+##################################################################
+############# prediction target y label     #######################
+
+# from 2009-04-08 2019-04-10
 
