95503c847641b4a544a6257277e98b55feffd5e8
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2021-01-29 11:43:28 -0500

    produced daily spectrum from 2009-2019

--- code/importData.R
+++ code/importData.R
@@ -378,8 +378,9 @@ SPX<- subset(SPX, lubridate::hour(SPX$DateTime)*60
               +lubridate::minute(SPX$DateTime) >= 9*60+30)
 SPX <- subset(SPX, lubridate::hour(SPX$DateTime)*60
               +lubridate::minute(SPX$DateTime) <= 16*60)
-
+head(SPX)
 SPX <- subset(SPX,DateTime >= " 2009-01-01 " & DateTime <= "2019-12-31")
+head(SPX)
 
 
 #length(SPX$DateTime)
@@ -397,6 +398,7 @@ day_index<-endpoints(spx_ts, on = "days", k = 1)
 
 # last min prices for each trading day
 lmP<-spx_ts[day_index,]
+head(lmP)
 
 # average prices from 1 to 360 mins
 n<-length(day_index)-1
@@ -437,36 +439,59 @@ for (i in 1:n ){
 mypath="//Users/luzhang/Desktop/spectrum"
 setwd(mypath)
 
-#Extract the data on 2009-01-02
-test<-spx_ts[1:day_index[2],]
-head(test)
-prices<-as.vector(test)
-
-DWTprice<-wavShrink(prices, wavelet="d4",
-                    n.level=1, #level=5 is too smoothy #change n.levels will get huge difference on spectrum plot
-                    shrink.fun="soft", thresh.fun="adaptive")
-
-#plot denoised power spectrum
-logReturn<-rep(0,length(DWTprice)-1)
-for (i in 1: (length(DWTprice)-1)){
-  logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+#avoid overwriting what files already exist @RockScience
+createNewFileName = function(path  = getwd(), pattern = "plot_of_something", extension=".png") {
+  myExistingFiles = list.files(path = path, pattern = pattern)
+  print(myExistingFiles)
+  completePattern = paste0("^(",pattern,")([0-9]*)(",extension,")$")
+  existingNumbers  = gsub(pattern = completePattern, replacement = "\\2", x = myExistingFiles)
+  
+  if (identical(existingNumbers, character(0)))
+    existingNumbers = 0
+  
+  return(paste0(pattern,max(as.numeric(existingNumbers))+1,extension))
 }
 
-my.data<-data.frame(x=logReturn)
-my.w<-analyze.wavelet(my.data, "x",
-                      loess.span = 0,
-                      dt=1,dj=1/250,
-                      lowerPeriod = 2,
-                      upperPeriod = 256,
-                      make.pval = FALSE,n.sim = 10)
+#test, Extract the data on 2009-01-02
+library(WaveletComp)
+library(wmtsa)
 
-#plot the wavelet power spectrum-prices
+n<-length(day_index)-1
+for (i in 1:n){
+  start<-day_index[i]+1
+  end<-day_index[i+1]
+  temp<-spx_ts[start:end,]
+  prices<-as.vector(temp)
+  DWTprice<-wavShrink(prices, wavelet="d4",
+                      n.level=1, 
+                      shrink.fun="soft", thresh.fun="adaptive")
+  logReturn<-rep(0,length(DWTprice)-1)
+  for (i in 1: (length(DWTprice)-1)){
+    logReturn[i]<-log(DWTprice[i+1])-log(DWTprice[i])
+  }
+  
+  my.data<-data.frame(x=logReturn)
+  my.w<-analyze.wavelet(my.data, "x",
+                        loess.span = 0,
+                        dt=1,dj=1/250,
+                        lowerPeriod = 2,
+                        upperPeriod = 256,
+                        make.pval = FALSE,n.sim = 10)
+  
+  #plot the wavelet power spectrum-prices
+  
+  png(filename = createNewFileName(pattern="sepctrum"))
+  wt.image(my.w, color.key = "interval",n.levels=250,
+           legend.params = list(lab="wavelet power levels",mar=4.7))
+  dev.off() 
+}
 
-png(file='spectrum.png',  width=600, height=320)
-image<-wt.image(my.w, color.key = "interval",n.levels=250,
-         legend.params = list(lab="wavelet power levels",mar=4.7))
 
-dev.off() 
+ 
+
+
+
+
 
 
 
